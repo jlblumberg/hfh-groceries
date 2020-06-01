ff906b13bafc41ab74742e64518163bf6fb5369c
Liz Daly <liz7daly@gmail.com>
2020-06-01 16:14:36 +0100

    Show map on page

---
+++ index.js
@@ -0,0 +1,6 @@
+function loadMap() {
+  var mapOptions = {zoom: 14, center: {lat: 51.509865, lng: -0.118092}, mapTypeId: 'roadmap'}
+
+  var map = new google.maps.Map(
+    document.getElementById('map'), mapOptions);
+}
