36951109c3f1f6a7b0ba5057b5ed232baf3e41bd
Haydon Houghton <haydonhoughton@outlook.com>
2020-06-01 16:46:08 +0100

    Add code to show marker on map - no API Key present

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function loadMap() {
-  var mapOptions = {zoom: 14, center: {lat: 51.509865, lng: -0.118092}, mapTypeId: 'roadmap'}
+
+  var startingPos = {lat: 51.509865, lng: -0.118092}
+  var mapOptions = {zoom: 14, center: startingPos, mapTypeId: 'roadmap'}
 
   var map = new google.maps.Map(
     document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker(
+    {position: startingPos, map: map}
+  );
+
+
 }
