d081f71322ef4d3a6ebc7ec060cdccba55ba1ffd
Jason <jasylwong@gmail.com>
2020-06-02 12:18:07 +0100

    add maps views

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-function loadMap(lat, long) {
-  var mapOptions = {zoom: 14, center: {lat: lat, lng: long}, mapTypeId: 'roadmap'}
+// function loadMap(lat, long) {
+//   var mapOptions = {zoom: 14, center: {lat: 51.509865, lng: -0.118092}, mapTypeId: 'roadmap'}
 
-  var map = new google.maps.Map(
-    document.getElementById('map'), mapOptions);
-  do
-}
-
-document.addEventListener('DOMContentLoaded', loadMap())
-
-<script async defer
-src="">
-</script>
-
-$( document ).ready(function() {
-  event.preventDefault();
-  var city = $('#current-city').val();
-  
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-    $('#current-temperature').text(data.main.temp);
-  })
-})   
+//   var map = new google.maps.Map(
+//   document.getElementById('map'), mapOptions);
+// }
