0ea01641bfe27c4f0017bafaf7c827e79e2bd060
Jason <jasylwong@gmail.com>
2020-06-02 21:27:26 +0100

    clean code style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,14 +9,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
 
 var infoWindow
 var map
@@ -46,7 +44,6 @@ window.loadMap = function(lat,lng) {
       map.setCenter(results[0].geometry.location);
     }
   });
-
 }
 
   function createMarker(place) {
@@ -56,14 +53,13 @@ window.loadMap = function(lat,lng) {
     });
 
     google.maps.event.addListener(marker, 'click', function() {
-      const openingHours = place.opening_hours.weekday_text.join("\n")
+      const openingHours = place.opening_hours.weekday_text.join(" | ")
       infoWindow.setContent(`<h3>${place.name}</h3><p>${place.formatted_address}</p><p>${openingHours}</p>`);
       infoWindow.open(map, this);
     });
   }
 
   function getPlaceDetails(place,callback) {
-
     var request = {
       placeId: place.place_id
     };
