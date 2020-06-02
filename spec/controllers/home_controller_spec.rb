6b76a42fa1dba43a8a882f9c42e01f10d4aba152
Sophia Bell <sophbell87@gmail.com>
2020-06-02 09:46:36 +0100

    Added bootstrap (#13)
    * updated readme
    
    * Revert "updated readme"
    
    This reverts commit e75349f6170d1aa7a8ae051f31c96b95cb3efc6a.
    
    * instantiating rails 6
    
    * adding rspec test suite and figaro gem for environmental variable
    
    * pointing rails towards javascript pack tag and webpacker
    
    * adding in maps controller and spec template
    
    * added home page
    
    * removal of maps helper line in maps controller
    
    * feature test template
    
    * travis
    
    * ruby version
    
    * adding home controller
    
    * commenting out tests to pass travis
    
    * adding bootstrap
    
    * home controller spec
    
    * merged master into this branch for travis changes
    
    * .gitignore coverage files
    
    * add README
    
    * add MVP section into README
    
    Co-authored-by: Jason <jasylwong@gmail.com>
    Co-authored-by: Jason Wong <50889506+jasylwong@users.noreply.github.com>

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe 'GET #index' do
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
